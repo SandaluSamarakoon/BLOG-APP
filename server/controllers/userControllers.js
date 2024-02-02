126e252955e691c5f1b31df30034008a026341e8
IT21276200 <89300501+IT21276200@users.noreply.github.com>
2024-02-03 00:43:15 +0530

    edeit

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -189,7 +189,44 @@ const changeAvatar = async (req, res , next) => {
 // POST ; api/users/register
 // PROTECTED
 const editUser = async (req, res , next) => {
-    
+    try{
+        const {name, email, currentPassword, newPassword, newConfirmPassword} = req.body;
+        if(!name || !email || !currentPassword || !newPassword) {
+            return next(new HttpError('Fill in all fields', 422))
+        }
+        // get user from database
+        const user = await User.findById(req.user.id);
+        if(!user) {
+            return next(new HttpError("User not found.", 403))
+        }
+
+        //make sure new email does't already exist
+        const emailExist = await User.findOne({email});
+        // we want to update other details with/without changing the email (which  is a unique id because we use it to login)
+        if(emailExist && (emailExist._id !== req.user.id)) {
+            return next(new HttpError("Email already exist.", 422));
+        }
+        // compare current password to db password
+        const validateUserPassword = await bcrypt.compare(currentPassword, user.password);
+        if(!validateUserPassword){
+            return next(new HttpError("Invalid current password", 422))
+        }
+
+        //compare new password
+        if(newPassword !== newConfirmPassword) {
+            return next(new HttpError("New Passwords do not match", 422))
+        }
+
+        // hash new password
+        const salt = await bcrypt.genSalt(10)
+        const hash = await bcrypt.hash(newPassword, salt);
+
+        // update user info in database
+        const newInfo = await User.findByIdAndUpdate(req.user.id, {name, email , password: hash}, {new: true})
+        res.status(200).json(newInfo)
+    } catch (error) {
+        return next(new HttpError(error))
+    }
 }
 
 
